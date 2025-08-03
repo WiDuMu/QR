4c89871bb868b7533a29d8a9fcb5ab3a27df6cc5
Duncan Murphy <97642890+WiDuMu@users.noreply.github.com>
2025-08-03 01:49:39 -0500

    saving progress

--- QRCode.js
+++ QRCode.js
@@ -1,28 +1,74 @@
-class QRCodeElement extends HTMLElement {
-	shadow;
-
-	constructor() {
-		super();
-		this.shadow = this.attachShadow({ mode: "closed" });
-	}
-
-	renderQR(text, border, bgColor, fgColor) {
-		if (border < 0) {
-			throw new RangeError("Border must be non-negative");
-		}
-		const qr = QRC.encodeText(text, QRC.Ecc.MEDIUM);
-		let parts = [];
-		for (let y = 0; y < qr.size; y++) {
-			for (let x = 0; x < qr.size; x++) {
-				if (qr.getModule(x, y))
-					parts.push(`M${x + border},${y + border}h1v1h-1z`);
-			}
-		}
-		this.shadow.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 ${qr.size + border * 2} ${qr.size + border * 2}" stroke="none">
-	<rect width="100%" height="100%" fill="${bgColor}"/>
-	<path d="${parts.join(" ")}" fill="${fgColor}"/>
-</svg>`;
-	}
+function randRange(maximum, minimum = 0) {
+   return Math.round(Math.random() * maximum) + minimum;
 }
 
-customElements.define("qr-code", QRCodeElement, {});
+// Utility functions
+function RGBtoHex(r, g, b) {
+   return `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`;
+}
+
+function randRGB() {
+   return RGBtoHex(randRange(255), randRange(255), randRange(255));
+}
+
+
+// Returns a string of SVG code for an image depicting the given QR Code, with the given number
+// of border modules. The string always uses Unix newlines (\n), regardless of the platform.
+function toSvgString(text, border, lightColor, darkColor) {
+   const qr = QRC.encodeText(text, QRC.Ecc.MEDIUM);
+	if (border < 0) throw new RangeError("Border must be non-negative");
+	const parts = [];
+	for (let y = 0; y < qr.size; y++) {
+		for (let x = 0; x < qr.size; x++) {
+			if (qr.getModule(x, y))
+				parts.push(`M${x + border},${y + border}h1v1h-1z`);
+		}
+	}
+	return `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 ${qr.size + border * 2} ${qr.size + border * 2}" stroke="none">
+	<rect width="100%" height="100%" fill="${lightColor}"/>
+	<path d="${parts.join(" ")}" fill="${darkColor}"/>
+</svg>
+`;
+}
+
+// Returns a string of SVG code for an image depicting the given QR Code, with adorable rainbow dots
+function toSvgStringCircle(text, border, lightColor) {
+   const qr = QRC.encodeText(text, QRC.Ecc.MEDIUM);
+	if (border < 0) throw new RangeError("Border must be non-negative");
+	const parts = [];
+	for (let y = 0; y < qr.size; y++) {
+		for (let x = 0; x < qr.size; x++) {
+			if (qr.getModule(x, y))
+				parts.push(`<ellipse style="fill: ${randRGB()}" cx=${x + border} cy=${y + border} rx="0.5px" ry="0.5px" />`);
+		}
+	}
+   return `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 ${qr.size + border * 2} ${qr.size + border * 2}" stroke="none">
+	<rect width="100%" height="100%" fill="${lightColor}"/>
+	${parts.join("")}
+</svg>
+`;
+}
+
+// Draws the given QR Code, with the given module scale and border modules, onto the given HTML
+// canvas element. The canvas's width and height is resized to (qr.size + border * 2) * scale.
+// The drawn image is purely dark and light, and fully opaque.
+// The scale must be a positive integer and the border must be a non-negative integer.
+function drawCanvas(text, scale, border, lightColor, darkColor, canvas) {
+   const qr = QRC.encodeText(text, QRC.Ecc.MEDIUM);
+	if (scale <= 0 || border < 0)
+		throw new RangeError("Value out of range");
+	const width = (qr.size + border * 2) * scale;
+	canvas.width = width;
+	canvas.height = width;
+	const ctx = canvas.getContext("2d");
+	for (let y = -border; y < qr.size + border; y++) {
+		for (let x = -border; x < qr.size + border; x++) {
+			ctx.fillStyle = qr.getModule(x, y) ? darkColor : lightColor;
+			ctx.fillRect((x + border) * scale, (y + border) * scale, scale, scale);
+		}
+	}
+}
--- index.js
+++ index.js
@@ -5,13 +5,13 @@ fgInput.value = "#000000";
 bgInput.value = "#FFFFFF";
 textInput.value = "";
 
-textInput.addEventListener("input", (e) =>
+textInput.addEventListener("input", () =>
 	showText(textInput.value, fgInput.value, bgInput.value),
 );
-fgInput.addEventListener("input", (e) =>
+fgInput.addEventListener("input", () =>
 	showText(textInput.value, fgInput.value, bgInput.value),
 );
-bgInput.addEventListener("input", (e) =>
+bgInput.addEventListener("input", () =>
 	showText(textInput.value, fgInput.value, bgInput.value),
 );
 
@@ -19,11 +19,9 @@ const QRC = qrcodegen.QrCode;
 const QR = document.getElementById("qr-code");
 const errorElement = document.getElementById("qr-code-errors");
 const QRDownload = document.getElementById("qr-download-button");
-console.log(QRDownload);
 const downloadLink = document.getElementById("secret-download-link");
 
-QRDownload.addEventListener("click", (e) => {
-	// console.log("hello");
+QRDownload.addEventListener("click", () => {
 	const encoded = encodeSVG(textInput.value, bgInput.value, fgInput.value);
 	const blob = new File([encoded], "qr.svg", { type: "image/svg+xml"});
 	const url = URL.createObjectURL(blob);
@@ -33,13 +31,12 @@ QRDownload.addEventListener("click", (e) => {
 });
 
 function showText(text, fg, bg) {
-	// console.log(text);
 	errorElement.replaceChildren();
 	try {
-		qr0 = QRC.encodeText(text, QRC.Ecc.MEDIUM);
-		QR.innerHTML = toSvgString(qr0, 2, bg ? bg : "white", fg ? fg : "black");
+		QR.innerHTML = toSvgStringCircle(text, 2, bg ? bg : "white", fg ? fg : "black");
 		QR.toggleAttribute("hidden", false);
 		QRDownload.toggleAttribute("hidden", false);
+		drawCanvas(text, 2, 2, bg, fg, canvas);
 	} catch {
 		QR.toggleAttribute("hidden", true);
 		QRDownload.toggleAttribute("hidden", true);
@@ -50,66 +47,4 @@ function showText(text, fg, bg) {
 	}
 }
 
-// // Draws the given QR Code, with the given module scale and border modules, onto the given HTML
-// // canvas element. The canvas's width and height is resized to (qr.size + border * 2) * scale.
-// // The drawn image is purely dark and light, and fully opaque.
-// // The scale must be a positive integer and the border must be a non-negative integer.
-// function drawCanvas(qr: qrcodegen.QrCode, scale: number, border: number, lightColor: string, darkColor: string, canvas: HTMLCanvasElement): void {
-// 	if (scale <= 0 || border < 0)
-// 		throw new RangeError("Value out of range");
-// 	const width: number = (qr.size + border * 2) * scale;
-// 	canvas.width = width;
-// 	canvas.height = width;
-// 	let ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-// 	for (let y = -border; y < qr.size + border; y++) {
-// 		for (let x = -border; x < qr.size + border; x++) {
-// 			ctx.fillStyle = qr.getModule(x, y) ? darkColor : lightColor;
-// 			ctx.fillRect((x + border) * scale, (y + border) * scale, scale, scale);
-// 		}
-// 	}
-// }
-
-function encodeSVG(text, bg, fg) {
-	const binary = QRC.encodeText(text, QRC.Ecc.MEDIUM);
-	const svgText = toSvgString(binary, 2, bg ? bg : "white", fg ? fg : "black");
-	return svgText;
-}
-
-// Returns a string of SVG code for an image depicting the given QR Code, with the given number
-// of border modules. The string always uses Unix newlines (\n), regardless of the platform.
-function toSvgString(qr, border, lightColor, darkColor) {
-	if (border < 0) throw new RangeError("Border must be non-negative");
-	const parts = [];
-	for (let y = 0; y < qr.size; y++) {
-		for (let x = 0; x < qr.size; x++) {
-			if (qr.getModule(x, y))
-				parts.push(`M${x + border},${y + border}h1v1h-1z`);
-		}
-	}
-	return `<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 ${qr.size + border * 2} ${qr.size + border * 2}" stroke="none">
-	<rect width="100%" height="100%" fill="${lightColor}"/>
-	<path d="${parts.join(" ")}" fill="${darkColor}"/>
-</svg>
-`;
-}
-
-function toSvgString2(qr, border, lightColor, darkColor) {
-	if (border < 0) throw new RangeError("Border must be non-negative");
-	const parts = [];
-	for (let y = 0; y < qr.size; y++) {
-		for (let x = 0; x < qr.size; x++) {
-			if (qr.getModule(x, y))
-				parts.push(`<ellipse style="fill: rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255});" cx=${x + border} cy=${y + border} rx="0.5px" ry="0.5px" />`);
-		}
-	}
-
-return `<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 ${qr.size + border * 2} ${qr.size + border * 2}" stroke="none">
-	<rect width="100%" height="100%" fill="${lightColor}"/>
-	${parts.join("")}
-</svg>
-`;
-}
+const canvas = document.getElementById("qr-canvas");
